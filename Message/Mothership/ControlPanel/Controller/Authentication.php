1057c967db7f2cf910f6dca09b0cd7efed88b27f
Joe Holdcroft <joe@message.co.uk>
2013-05-23 13:27:22 +0100

    Bit of a tidy up

--- Message/Mothership/ControlPanel/Controller/Authentication.php
+++ Message/Mothership/ControlPanel/Controller/Authentication.php
@@ -7,37 +7,18 @@ use Message\Mothership\CMS\Page\Authorisation;
 use Message\Cog\ValueObject\DateRange;
 
 class Authentication extends \Message\Cog\Controller\Controller
-{	
+{
 	public function login()
 	{
-		$page = new Page;
-		$page->id = 1;
-		$page->password = 'hello';
-		$page->publishState = 1;
-		$from = new \DateTime('+1 week');
-		$to = new \DateTime('+1 week');
-		$page->publishDateRange = new DateRange($from, $to);
-		
-		$user = array();
-
-		$authorisation = new Authorisation($page,$user);
-		$password = 'hello';
-		
-		$authorisation->validatePassword($password);
-		var_dump($authorisation->isPublished()); exit;
-		var_dump($authorisation); exit;
-	
-	
 		$session = $this->_services['http.session'];
-		$user = $session->get('user');
-		$return = array(
-			'user' => $user,
-			'logout' => false,
-		);
+		$user    = $session->get('user');
 
-		return $this->render('::login', $return);
+		return $this->render('::login', array(
+			'user'   => $user,
+			'logout' => false,
+		));
 	}
-	
+
 	public function loginAction()
 	{
 		// Check we have some post data, otherwsie redirect back to the login page
@@ -45,37 +26,30 @@ class Authentication extends \Message\Cog\Controller\Controller
 
 			// At this stage we need to check the post data is all there
 			// Then we need to ensure that there is a username and password actually match
-			$session = $this->_services['http.session'];
 			$user = $this->_services['user'];
-			$data = array(
-				'id'		=> 69,
-				'forename' 	=> 'Danny',
-				'surname'	=> 'Hannah',
-				'email'		=> 'danny@message.co.uk',
-			);
+			$user->id       = 69;
+			$user->forename = 'Danny';
+			$user->surname  = 'Hannah';
+			$user->email    = 'danny@message.co.uk';
 
 			if (isset($post['remember']) && $post['remember']) {
-				
-				$cookie = new \Message\Cog\HTTP\Cookie('user_id',69);				
-				$this->_services['http.cookies']->add($cookie);
-
+				$this->_services['http.cookies']->add(
+					new \Message\Cog\HTTP\Cookie('user_id', $user->id)
+				);
 			}
 
-			$user->load($data);
-			$session->set('user',$user);
+			$this->_services['http.session']->set('user', $user);
 
 			return $this->render('::admin', $data);
-		} else {
-			// Redirect back
-			$this->redirect($this->generateUrl('ms.cp.login'));
 		}
+
+		return $this->redirect($this->generateUrl('ms.cp.login'));
 	}
 
 	public function logout()
 	{
-		$session = $this->_services['http.session'];
-		$session->remove('user');
+		$this->_services['http.session']->remove('user');
+
 		return $this->redirect($this->generateUrl('ms.cp.login'));
 	}
-
 }
