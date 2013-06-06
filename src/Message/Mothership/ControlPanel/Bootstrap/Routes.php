2699561d64bb3eb76317efb22f3c28f32015ba29
Joe Holdcroft <joe@message.co.uk>
2013-06-06 12:12:51 +0100

    Extending Message\User login

--- src/Message/Mothership/ControlPanel/Bootstrap/Routes.php
+++ src/Message/Mothership/ControlPanel/Bootstrap/Routes.php
@@ -8,8 +8,10 @@ class Routes implements RoutesInterface
 {
 	public function registerRoutes($router)
 	{
-		$router->add('ms.cp.login', '/login', '::Controller:Authentication#login');
-		$router->add('ms.cp.logout', '/logout', '::Controller:Authentication#logout');
-		$router->add('ms.cp.login.action', '/login/action', '::Controller:Authentication#loginAction');
+		$router['ms.cp']->setPrefix('/admin');
+
+		$router['ms.cp']->add('ms.cp.dashboard', '/dashboard', '::Controller:Dashboard#index');
+		$router['ms.cp']->add('ms.cp.login', '/login', '::Controller:Authentication#login');
+		$router['ms.cp']->add('ms.cp.logout', '/logout', '::Controller:Authentication#logout');
 	}
 }
--- src/Message/Mothership/ControlPanel/Controller/Authentication.php
+++ src/Message/Mothership/ControlPanel/Controller/Authentication.php
@@ -10,46 +10,12 @@ class Authentication extends \Message\Cog\Controller\Controller
 {
 	public function login()
 	{
-		$session = $this->_services['http.session'];
-		$user    = $session->get('user');
-
-		return $this->render('::login', array(
-			'user'   => $user,
-			'logout' => false,
-		));
-	}
-
-	public function loginAction()
-	{
-		// Check we have some post data, otherwsie redirect back to the login page
-		if ($post = $this->_services['request']->request->get('login')) {
-
-			// At this stage we need to check the post data is all there
-			// Then we need to ensure that there is a username and password actually match
-			$user = $this->_services['user'];
-			$user->id       = 69;
-			$user->forename = 'Danny';
-			$user->surname  = 'Hannah';
-			$user->email    = 'danny@message.co.uk';
-
-			$this->_services['http.session']->set('user', $user);
-
-			// Set cookie if the user wants to be remembered
-			if (isset($post['remember']) && $post['remember']) {
-				$this->_services['http.cookies']->add(
-					new \Message\Cog\HTTP\Cookie('user_id', $user->id)
-				);
-			}
-
-			return $this->render('::admin', $data);
-		}
-
-		return $this->redirect($this->generateUrl('ms.cp.login'));
+		return $this->render('::login');
 	}
 
 	public function logout()
 	{
-		$this->_services['http.session']->remove('user');
+		#$this->_services['http.session']->remove('user');
 
 		return $this->redirect($this->generateUrl('ms.cp.login'));
 	}
