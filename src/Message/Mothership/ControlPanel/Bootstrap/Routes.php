d33c6940cb5a1cf0b6ee0cad74f38a56c7397afe
Joe Holdcroft <joe@message.co.uk>
2013-06-20 19:58:36 +0100

    Bug fix

--- src/Message/Mothership/ControlPanel/Bootstrap/Routes.php
+++ src/Message/Mothership/ControlPanel/Bootstrap/Routes.php
@@ -14,10 +14,10 @@ class Routes implements RoutesInterface
 		$router['ms.cp.external']->add('ms.cp.login', '/login', '::Controller:Authentication#login');
 		$router['ms.cp.external']->add('ms.cp.logout', '/logout', '::Controller:Authentication#logout');
 
-		$router['ms.cp']->add('ms.cp.dashboard', '/', '::Controller:Dashboard#index');
+		$router['ms.cp.external']->add('ms.cp.dashboard', '/', '::Controller:Dashboard#index');
 
-		$router['ms.cp']->add('ms.cp.password.request', '/password/request', '::Controller:ForgottenPassword#request');
-		$router['ms.cp']->add('ms.cp.password.request.email_prefill', '/password/request/{email}', '::Controller:ForgottenPassword#request');
-		$router['ms.cp']->add('ms.cp.password.reset', '/password/reset/{email}/{hash}', '::Controller:ForgottenPassword#reset');
+		$router['ms.cp.external']->add('ms.cp.password.request', '/password/request', '::Controller:ForgottenPassword#request');
+		$router['ms.cp.external']->add('ms.cp.password.request.email_prefill', '/password/request/{email}', '::Controller:ForgottenPassword#request');
+		$router['ms.cp.external']->add('ms.cp.password.reset', '/password/reset/{email}/{hash}', '::Controller:ForgottenPassword#reset');
 	}
 }
