2768a1fd90362b755d387c7c7d9753e3ad7ff186
Joe Holdcroft <joe@message.co.uk>
2013-07-04 12:12:51 +0100

    Adding live pane quick links!

---
+++ src/Message/Mothership/ControlPanel/View/_assets/js/livePane.jquery.js
@@ -0,0 +1,102 @@
+/**
+ * "Live Pane" jQuery Plugin
+ *
+ * Provides functionality to load in the contents of a specific area, or "pane"
+ * of a page dynamically without a page refresh.
+ *
+ * This is a *private* plugin, and should only be used by Message Digital Design.
+ *
+ * @author Message Digital Design <dev@message.co.uk>
+ * @author Joe Holdcroft <joe@message.co.uk>
+ */
+(function( $ ){
+	var methods = {
+		/**
+		 * Initialise the plugin.
+		 *
+		 * @param object options Object of options for this plugin
+		 */
+		init : function(options) {
+			var defaults = {
+					linkSelector: false, // Selector string for links that should load in the pane
+					beforeSend  : false, // Event for before the ajax request is made
+					afterSend   : false  // Event for when the ajax response is received
+				},
+				settings = $.extend({}, defaults, options),
+				selector = this.selector;
+
+			return this.each(function() {
+				var self  = $(this),
+					state = {
+						settings: settings,
+						selector: selector
+					};
+
+				// Save state on the element for use later
+				self.data('livePane', state);
+
+				// Bind the click & form submit events
+				methods.bindEvents.call(self);
+			});
+		},
+
+		bindEvents : function() {
+			// Set up history event listener for loading content
+			History.Adapter.bind(window, 'statechange.livePane', function() {
+				methods.loadUri.call(this, History.getState().url);
+			});
+
+			return this.each(function() {
+				var self = $(this);
+
+				// Set up click event for live links
+				if (self.data('livePane').settings.linkSelector != false) {
+					$(document).on('click.livePane', self.data('livePane').settings.linkSelector, function(e) {
+						History.pushState(null, null, $(this).attr('href'));
+
+						return false;
+					});
+				}
+			});
+		},
+
+		loadUri : function(uri) {
+			return this.each(function() {
+				var self  = $(this),
+					state = self.data('livePane');
+
+				// If set, fire the beforeSend event
+				if (typeof state.settings.beforeSend === 'function') {
+					state.settings.beforeSend(self);
+				}
+
+				$.ajax({
+					url     : uri,
+					dataType: 'html',
+					complete: function() {
+						// If set, fire the afterSend event
+						if (typeof state.settings.afterSend === 'function') {
+							state.settings.afterSend(self);
+						}
+					},
+					success : function(html) {
+						self.html($(state.selector, html).html());
+
+						History.pushState(null, null, uri);
+					}
+				});
+			});
+		}
+	};
+
+	$.fn.livePane = function(method) {
+		// Method calling logic
+		if (methods[method]) {
+			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+		} else if (typeof method === 'object' || !method) {
+			return methods.init.apply(this, arguments);
+		} else {
+			$.error('Method ' +  method + ' does not exist on jQuery.livePane');
+		}
+	};
+})(jQuery);
--- src/Message/Mothership/ControlPanel/View/_assets/js/setup.js
+++ src/Message/Mothership/ControlPanel/View/_assets/js/setup.js
@@ -1,7 +1,41 @@
 $(function() {
 	$('html').removeClass('no-js').addClass('js');
 
+	if (!History.enabled) {
+		$.error('Your browser does not support the HTML5 History API, so the control panel may not function correctly.');
+	}
+
 	$('[data-confirm]').on('click submit', function() {
 		return confirm($(this).attr('data-confirm'));
+	});
+
+	$('[data-live-pane]').livePane({
+		linkSelector: 'a[data-live]',
+		beforeSend: function(pane) {
+			$('html').addClass('loading');
+
+			// Disable form inputs
+			pane.find('input, select, textarea, button').each(function() {
+				var self = $(this);
+
+				if (self.attr('disabled')) {
+					self.attr('data-disabled', true);
+				}
+
+				self.attr('disabled', 'disabled');
+			});
+		},
+		afterSend: function(pane) {
+			$('html').removeClass('loading');
+
+			// Re-enable form inputs
+			pane.find('input, select, textarea, button').each(function() {
+				var self = $(this);
+
+				if (!self.attr('data-disabled')) {
+					self.removeAttr('disabled');
+				}
+			});
+		}
 	});
 });
