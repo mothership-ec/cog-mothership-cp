5a07604a118487b20b67479123553427e68a297b
Joe Holdcroft <joe@message.co.uk>
2013-07-07 16:11:58 +0100

    Updating use of current user in templates following messagedigital/cog-user@46d6bc6b20dccdf82c654bc8021f6e2a813a51f8

--- src/Message/Mothership/ControlPanel/Controller/Dashboard.php
+++ src/Message/Mothership/ControlPanel/Controller/Dashboard.php
@@ -6,8 +6,6 @@ class Dashboard extends \Message\Cog\Controller\Controller
 {
 	public function index()
 	{
-		return $this->render('::dashboard', array(
-			'user' => $this->get('user.current'),
-		));
+		return $this->render('::dashboard');
 	}
 }
