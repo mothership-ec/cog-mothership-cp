215e41bddc8047bb455c095ec6ed07b85ae3cba7
Richard McCartney <richard.mccartney@me.com>
2013-07-23 17:15:27 +0100

    contextual help and updating the font awesome files.

---
+++ src/Message/Mothership/ControlPanel/View/_assets/js/title.js
@@ -0,0 +1,37 @@
+/**
+ * Title Edit
+ *
+ * JS to edit the CMS title of a page
+ *
+ * @author Message Digital Design <dev@message.co.uk>
+ * @author Richard McCartney <richard@message.co.uk>
+ */
+
+$(function() {
+
+	var title = '[data-title]',
+		edit = '[data-title-edit]';
+
+
+	$(document).on('click', function(e) {
+		
+		// If the title is the target show the edit input
+		if ( $(e.target).is(title) ) {
+			
+			$(edit).show();
+
+		// stop clicking
+		} else if ( $(e.target).is(edit) ) {
+
+			e.stopPropagation();
+
+		} else {
+
+			$(edit).hide();
+
+		}
+
+	});
+
+});
+
