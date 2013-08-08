2c065bb3f8c9d5c0ed49491d21602fe3f9153d2b
Richard McCartney <richard.mccartney@me.com>
2013-08-08 13:58:45 +0100

    feedback js

--- src/Message/Mothership/ControlPanel/View/_assets/js/feedback.js
+++ src/Message/Mothership/ControlPanel/View/_assets/js/feedback.js
@@ -11,15 +11,44 @@
 
 $(function() {
 
-	var feedback  = '.feedback',
-		height	  = 0,
-		container = '.container-content';
+	var feedback    = '.feedback',
+		height      = 0,
+		offSet      = 120,
+		container   = '.container-content',
+		saveButton  = '#save-content';
 
+
+	// Check if its the filemanager 
+	function checkContainer() {
+
+		if ( $(container).hasClass('file-manager') || $(container).hasClass('create-page')) {
+	
+			offSet = 65;
+
+		} else {
+
+			offSet = 120;
+
+		}
+
+	};
+
+
+	// If the feedback is visible work out offset
 	if ( $(feedback).is(':visible')) {
+
+		// Check the container first
+		checkContainer();
 
 		height = $(feedback).outerHeight();
 
-		console.log(height);
+		offSet = offSet + height;
+
+		$(container).css({
+			width: 'calc(100% - 200px)',
+			width: 'calc(100% - 200px)',
+			width: 'calc(100% - 200px)'
+		});
 
 	} else {
 
