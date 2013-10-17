7d7fabc86d9a26feaf5a7cfe28091827bae0a0ce
Laurence Roberts <lsjroberts@gmail.com>
2013-10-17 16:53:11 +0100

    Added label replace for group titles

--- resources/assets/js/forms.js
+++ resources/assets/js/forms.js
@@ -6,10 +6,13 @@
 ;$(function() {
 	// Set up add links for repeatable groups
 	$(document).on('click', 'a[data-group-add]', function() {
-		var self          = $(this),
-			index         = self.attr('data-group-index') || 0,
-			prototypeName = self.attr('data-prototype-name') || '__name__',
-			prototype     = self.attr('data-prototype').replace(new RegExp(prototypeName, 'g'), index);
+		var self           = $(this),
+			index          = self.attr('data-group-index') || 0,
+			prototypeName  = self.attr('data-prototype-name') || '__name__',
+			prototypeLabel = self.attr('data-prototype-label') || '__label__',
+			prototype      = self.attr('data-prototype').replace(new RegExp(prototypeName, 'g'), index);
+
+		prototype = prototype.replace(new RegExp(prototypeLabel, 'g'), parseInt(index) + 1);
 
 		self.before($(prototype).hide().fadeIn(200));
 
