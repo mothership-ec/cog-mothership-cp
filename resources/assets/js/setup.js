11e461c63552aaf9844dfbccfcadfe7104da9d4d
Richard McCartney <richard@message.co.uk>
2014-02-26 12:53:43 +0000

    save buttons will now not overlap on screen resize

--- resources/assets/js/setup.js
+++ resources/assets/js/setup.js
@@ -1,3 +1,19 @@
 ;$(function() {
 	$('html').removeClass('no-js').addClass('js');
+
+
+$(window).resize(function(){
+	var windowWidth = $(window).width();
+
+	$("#test").html(windowWidth);//Just to see in real time what's the window with
+
+	if (windowWidth < 1260) {
+    	$('#save-content, .save-content').css('left', '1093px');
+  	}
+	else {
+   		$('#save-content, .save-content').css('right', '14px');
+	    $('#save-content, .save-content').css('left', 'auto');
+	  }
+});
+
 });
