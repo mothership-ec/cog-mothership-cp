b89383d195e03201623c79b7825aa5dd8061687b
Richard McCartney <richard@message.co.uk>
2014-03-05 09:53:45 +0000

    button amendments

--- resources/assets/js/setup.js
+++ resources/assets/js/setup.js
@@ -3,17 +3,22 @@
 
 
 $(window).resize(function(){
-	var windowWidth = $(window).width();
+	var windowWidth = $(window).width(),
+		buttonWidth = $('#save-content').outerWidth()
+		offSet = 0;
 
 	$("#test").html(windowWidth);//Just to see in real time what's the window with
 
 	if (windowWidth < 1260) {
-    	$('#save-content, .save-content').css('left', '1093px');
+		offSet = 1246 - buttonWidth;
+    	$('#save-content, .save-content').css('left', offSet);
+
+    	console.log(offSet, topBar, buttonWidth);
   	}
 	else {
    		$('#save-content, .save-content').css('right', '14px');
 	    $('#save-content, .save-content').css('left', 'auto');
-	  }
+	}
 });
 
 });
