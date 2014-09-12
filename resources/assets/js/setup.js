af8d2aa8a72abccede212ac36f14f332df71ff52
Richard McCartney <richard@message.co.uk>
2014-09-12 16:17:13 +0100

    Table filter JS

--- resources/assets/js/setup.js
+++ resources/assets/js/setup.js
@@ -26,6 +26,32 @@ $(window).resize(function(){
 		gutter: 20
 	});
 
+	// Product table JS setup
+	$('.table-filter.products').dataTable({
+		iDisplayLength: 25,
+		"oLanguage": {
+			"sLengthMenu": 'Display <select>'+
+			'<option value="25">25</option>'+
+			'<option value="50">50</option>'+
+			'<option value="100">100</option>'+
+			'<option value="200">200</option>'+
+			'<option value="-1">All</option>'+
+			'</select> products',
+		"sInfo": "Showing (_START_ to _END_) of _TOTAL_ Products"}
+    }).columnFilter({
+		aoColumns: [
+			{ type: "text" },
+			null,
+			{ type: "text" },
+			{ type: "text" },
+			null
+		]
+	});
+
+	$(document).ready( function() {
+		$('.table-filter.products').dataTable();
+	});
+
 });
 
 });
