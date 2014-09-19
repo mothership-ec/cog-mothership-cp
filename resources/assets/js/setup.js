7aa3bb968e2967bb36169dbb0ab378cb9685e1c6
Samuel Trangmar-Keates <sam@message.co.uk>
2014-09-19 16:38:33 +0100

    JS moved onto module

--- resources/assets/js/setup.js
+++ resources/assets/js/setup.js
@@ -27,53 +27,8 @@
 		});
 	});
 
-	// Product table JS setup
-	$('.table-filter.products').dataTable({
-		iDisplayLength: 25,
-		"oLanguage": {
-			"sLengthMenu": 'Display <select>'+
-			'<option value="25">25</option>'+
-			'<option value="50">50</option>'+
-			'<option value="100">100</option>'+
-			'<option value="200">200</option>'+
-			'<option value="-1">All</option>'+
-			'</select> products',
-		"sInfo": "Showing (_START_ to _END_) of _TOTAL_ Products"}
-    }).columnFilter({
-		aoColumns: [
-			{ type: "text" },
-			null,
-			{ type: "text" },
-			{ type: "text" },
-			null
-		]
-	});
-
-	// CMS table JS setup
-	$('.table-filter.pages').dataTable({
-		iDisplayLength: 25,
-		"oLanguage": {
-			"sLengthMenu": 'Display <select>'+
-			'<option value="25">25</option>'+
-			'<option value="50">50</option>'+
-			'<option value="100">100</option>'+
-			'<option value="200">200</option>'+
-			'<option value="-1">All</option>'+
-			'</select> pages',
-		"sInfo": "Showing (_START_ to _END_) of _TOTAL_ Pages"}
-    }).columnFilter({
-		aoColumns: [
-			{ type: "text" },
-			null,
-			{ type: "text" },
-			{ type: "text" },
-			null
-		]
-	});
-
 	$(document).ready( function() {
-		$('.table-filter.products').dataTable();
-		$('.table-filter.pages').dataTable();
+		$('.table-filter.page').dataTable();
 	});
 
 
