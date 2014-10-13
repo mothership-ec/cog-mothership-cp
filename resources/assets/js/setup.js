e492941539b7898433c0814d630739bd70022b07
Aisling Brock <aisling@message.co.uk>
2014-10-13 17:15:54 +0100

    Product CSV upload styling

--- resources/assets/js/setup.js
+++ resources/assets/js/setup.js
@@ -2,6 +2,29 @@
 	$('html').removeClass('no-js').addClass('js');
 
 
+$( ".csv-show.valid" ).click(function() {
+	if ($('.valid .hide').is(":visible")) {
+		$('.valid .hide').hide();
+		$( '.csv-show.valid').text("Show all rows");
+	} 
+	else {
+		$('.valid .hide').show();
+		$( '.csv-show.valid').text("Hide rows");
+	}
+});
+
+$( ".csv-show.invalid" ).click(function() {
+	if ($('.invalid .hide').is(":visible")) {
+		$('.invalid .hide').hide();
+		$( '.csv-show.invalid').text("Show all rows");
+
+	} 
+	else {
+		$('.invalid .hide').show();
+		$( '.csv-show.invalid').text("Hide rows");
+	}
+});
+
 $(window).resize(function(){
 	var windowWidth = $(window).width(),
 		buttonWidth = $('#save-content').outerWidth()
